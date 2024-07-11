b5bd2bac2c9a4a73818fe642b4a605b2c5e036f3
FarmerJohnsBessie <cz52013141834@gmail.com>
2024-07-11 10:15:32 -0400

    Added files

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Features from './pages/Features';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
